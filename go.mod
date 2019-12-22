02d656fa0d8cb2636c1346d0cc141572c739ad70
nownabe <nownabe@gmail.com>
2019-12-22 20:52:46 +0900

    :tada: Add basic functionalities

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/nownabe/discord-vc-notifier
 go 1.13
 
 require (
+	github.com/bwmarrin/discordgo v0.20.1
 	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915 // indirect
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
 )
--- main.go
+++ main.go
@@ -5,20 +5,75 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/bwmarrin/discordgo"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "ok")
-	})
+type notifier interface {
+	notify(string)
+}
 
+type discordNotifier struct {
+	channelID string
+	session   *discordgo.Session
+}
+
+func (n *discordNotifier) notify(msg string) {
+	if _, err := n.session.ChannelMessageSend(n.channelID, msg); err != nil {
+		log.Printf("failed to send message to discord: %v", err)
+	}
+}
+
+func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "ok")
+	})
+
+	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
+	if err != nil {
+		log.Fatalf("failed to build discord: %v", err)
+	}
+
+	notifiers := []notifier{
+		&discordNotifier{channelID: "658184345382551563", session: discord},
+	}
+
+	discord.AddHandler(handler(notifiers))
+
+	if err := discord.Open(); err != nil {
+		log.Fatalf("failed to open discord: %v", err)
+	}
+	defer discord.Close()
+
+	log.Printf("Opened discord")
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
+	}
+	log.Printf("Terminating...")
+}
+
+func handler(ns []notifier) func(*discordgo.Session, *discordgo.VoiceStateUpdate) {
+	return func(s *discordgo.Session, vsu *discordgo.VoiceStateUpdate) {
+		if vsu.VoiceState.ChannelID == "" {
+			return
+		}
+
+		m, err := s.GuildMember(vsu.VoiceState.GuildID, vsu.VoiceState.UserID)
+		if err != nil {
+			log.Printf("failed to get member: %v", err)
+			return
+		}
+
+		msg := fmt.Sprintf("%sがボイチャにいるよ", m.Nick)
+
+		for _, n := range ns {
+			n.notify(msg)
+		}
 	}
 }
