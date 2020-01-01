66537d36095d0451d88d63853b63b2f7689d453c
nownabe <nownabe@gmail.com>
2020-01-01 11:19:03 +0900

    :bug: Use config to set project

--- .github/actions/deploy-gae/entrypoint.sh
+++ .github/actions/deploy-gae/entrypoint.sh
@@ -4,6 +4,8 @@ set -eu
 
 echo ${INPUT_CREDENTIALS} > credentials.json
 gcloud auth activate-service-account --key-file credentials.json
-gcloud app deploy --project ${INPUT_PROJECT_ID} --quiet
+gcloud config set project ${INPUT_PROJECT_ID}
+
+gcloud app deploy --quiet
 old_versions=$(gcloud app versions list --filter=traffic_split=0.00 --format='value(id)' | tr '\n' ' ')
 gcloud app versions delete --quiet $old_versions
